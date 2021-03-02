c0bf895744897d3fe55ce10e72647622d40cce93
ghassen92 <guesmighassen@yahoo.com>
2021-03-02 19:08:32 +0100

    should_return_fuzz_for_3 refatoring

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -3,7 +3,8 @@ package com.example.demo;
 public class FizzBuzz {
 
     public static String getResult(int input) {
+        String fuzz = "Fuzz";
         if (input <= 0) throw new RuntimeException("Input should be greator then 0");
-        return "Fuzz";
+        return fuzz;
     }
 }
