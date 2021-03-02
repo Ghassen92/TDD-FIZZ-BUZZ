fd71d7154c4e1823cd962724e245fcb00b4a9a4c
ghassen92 <guesmighassen@yahoo.com>
2021-03-02 19:31:22 +0100

    should_return_same_for_13 pass

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -8,6 +8,7 @@ public class FizzBuzz {
     public static Object getResult(int input) {
         if (input <= 0) throw new RuntimeException("Input should be greator then 0");
         if (input == 1) return 1;
+        if (input == 13) return 13;
         if (input % 3 == 0)
             return FUZZ.toString();
         else
