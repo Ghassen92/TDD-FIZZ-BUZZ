f8261a5ae6cd6a2974d856ce149966b25c6df9a8
ghassen92 <guesmighassen@yahoo.com>
2021-03-02 18:59:03 +0100

    should_throw_exception_for_minus4 refactoring

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -3,6 +3,6 @@ package com.example.demo;
 public class FizzBuzz {
 
     public static void getResult(int input) {
-        if (input == -1 || input == 0 || input == -4) throw new RuntimeException("Input should be greator then 0");
+        if (input <= 0) throw new RuntimeException("Input should be greator then 0");
     }
 }
