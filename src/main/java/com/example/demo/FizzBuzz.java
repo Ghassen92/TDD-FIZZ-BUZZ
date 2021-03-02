c76fd5ec80f3c990d03882191875a70c916010e7
ghassen92 <guesmighassen@yahoo.com>
2021-03-02 19:14:27 +0100

    should_return_fuzz_for_6 pass

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -4,7 +4,11 @@ public class FizzBuzz {
 
     public static String getResult(int input) {
         if (input <= 0) throw new RuntimeException("Input should be greator then 0");
-        String output = input == 3 ? "Fuzz" : "Buzz";
+        String output;
+        if (input == 3 | input == 6)
+            output = "Fuzz";
+        else
+            output = "Buzz";
         return output;
     }
 }
